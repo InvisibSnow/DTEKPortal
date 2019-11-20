e1d470e04c9618e84c15eb68bd1bb2a86ee80d9c
Pursheha Ivan <ivan.pursheha@gmail.com>
2019-11-20 11:36:23 +0200

    Initial commit

---
+++ app/src/main/java/com/dtek/portal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dtek.portal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
